a3626b2944c618dfb1380c7f7f7504f8c1050389
Alexander Leitzke <78230669+ajleitzke@users.noreply.github.com>
2023-02-15 13:43:05 -0600

    Consolidate to one file and allow modification of save directory

--- main.py
+++ main.py
@@ -1,21 +1,68 @@
-import install
-import save
 from colorama import Fore
+import pickle
+import os
 
-# Prompt user what to do
-def prompt():
-    do = input(Fore.RESET + "What would you like to do today?\nS = Save, I = Install, Q = Quit\n")
-    if do not in ["S", "I", "Q"]:
-        print(Fore.RED + "You have entered an invalid option. Please try again...")
-        prompt()
-    elif do == "Q":
-        print(Fore.RED + "Quitting...")
-        exit()
-    elif do == "S":
-        save.run()
-    elif do == "I":
-        install.install()
+
+class Backup:
+    def __init__(self):
+        self.varStorage = ".storage/var_storage.pk"
+        # Get preferred file save location, set to default if none
+        try:
+            self.saveLocation = pickle.load(open(self.varStorage, "rb"))
+        except EOFError:
+            self.saveLocation = ".storage/"
+        self.fileLocation = self.saveLocation + "brew-list.txt"
+        self.prompt()
+
+    # Prompt user to select an option
+    def prompt(self):
+        do = input(
+            Fore.RESET + "What would you like to do today?\n=================================\nS = Save package list\nI = Install packages from list\nM = Modify Save Location\nQ = Quit program\n=================================\n")
+        if do not in ["S", "I", "M", "Q"]:
+            print(Fore.RED + "You have entered an invalid option. Please try again...")
+            Backup.prompt(self)
+        elif do == "Q":
+            print(Fore.RED + "Quitting...")
+            exit()
+        elif do == "M":
+            Backup.modify(self)
+        elif do == "S":
+            Backup.save(self)
+        elif do == "I":
+            Backup.install(self)
+        return self.saveLocation, self.fileLocation
+
+    # Modify the save location and store it for future program use
+    def modify(self):
+        self.saveLocation = input(
+            "\nEnter a valid save path (default: .storage/):\n" + "Current save location is: " + self.saveLocation + "\n")
+        with open(self.varStorage, "wb") as file:
+            pickle.dump(self.saveLocation, file)
+        self.fileLocation = self.saveLocation + "brew-list.txt"
+        Backup.prompt(self)
+
+    # Create required directories and files if needed
+    def initialize(self):
+        if not os.path.exists(self.saveLocation):
+            os.makedirs(self.saveLocation)
+
+    # Save list of installed packages in homebrew
+    def saveBrewInstallFile(self):
+        os.system("brew leaves > " + self.fileLocation)
+        print(Fore.GREEN + "Package list saved!\n")
+        Backup.prompt(self)
+
+    def save(self):
+        Backup.initialize(self)
+        Backup.saveBrewInstallFile(self)
+
+    # Install programs from saved list
+    def install(self):
+        installList = open(self.fileLocation, "r").read().replace("\n", " ")
+        os.system("brew install " + installList)
+        print(Fore.GREEN + "Install complete!\n")
+        Backup.prompt(self)
 
 
 if __name__ == '__main__':
-    prompt()
+    Backup()
